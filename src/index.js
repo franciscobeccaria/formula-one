ccdf400495e6ffac3736a307ea0f006187da6b94
Francisco Beccaria <fbeccaria24@gmail.com>
2020-12-18 17:49:01 -0300

    Se realizaron los estilos del header.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import Router from './router.js';
 
 console.log(Router);
+
+const headerNav = document.getElementById('main-header-nav');
+const closeMenu = document.getElementById('close-menu');
+const toggleMenu = document.getElementById('toggle-menu');
+
+toggleMenu.addEventListener('click', () => {
+  headerNav.classList.add('show-modal-menu');
+});
+
+closeMenu.addEventListener('click', () => {
+  headerNav.classList.remove('show-modal-menu');
+});
